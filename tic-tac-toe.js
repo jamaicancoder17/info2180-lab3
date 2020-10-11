22c7290f12912be513dbf113a155a593f63d15a6
Marlon Williams <marlon.kingson@gmail.com>
2020-10-10 20:40:56 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,39 @@
 "use strict";
+
+var main = function(){
+    var count = 0;
+    let boardState = ["","","","","","","","",""];
+    var cube  = document.querySelectorAll(".square");
+    
+    for (var number = 0; number < cube.length; number++){
+        cube[number].addEventListener("click", function(event){
+            event.preventDefault();
+            if (count % 2 == 0){
+                console.log("You have pressed 'X'");
+                event.target.classList.add("X");
+                event.target.textContent = "X";
+                boardState[event.target.id] = "X";
+            } 
+            else{
+                console.log("You have pressed 'O'");
+                event.target.classList.add("O");
+                event.target.textContent = "O";
+                boardState[event.target.id] = "O"
+            }
+            count = count + 1; 
+        });
+
+}
+    console.log("Finished setting up Event Handlers");
+
+}
+
 window.onload =  function() {
     var x  = document.querySelectorAll("#board div");
     for (var number = 0; number < x.length; number++){
         x[number].className = "square";
+        x[number].id = number;
     }
+    console.log("Finished Setup");
+    main();
 }
